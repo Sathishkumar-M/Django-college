c6ebf3d9b27efbffdc991eec19f6d4036b859327
sathish-ku-mar <sathishkumar.appiness@gmail.com>
2018-03-15 17:56:04 +0530

    changes

--- college/college/settings.py
+++ college/college/settings.py
@@ -158,13 +158,13 @@ JWT_AUTH = {
     'JWT_VERIFY': True,
     'JWT_VERIFY_EXPIRATION': True,
     'JWT_LEEWAY': 0,
-    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=3000),
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=300),
     'JWT_AUDIENCE': None,
     'JWT_ISSUER': None,
 
     'JWT_ALLOW_REFRESH': False,
     'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
 
-    'JWT_AUTH_HEADER_PREFIX': 'Latha',
+    'JWT_AUTH_HEADER_PREFIX': 'Paapa_typing___!',
     'JWT_AUTH_COOKIE': None,
 }
